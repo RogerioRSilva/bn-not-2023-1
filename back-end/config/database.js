90c4ed448ae7dbc4e82f8aa5cdce1c9a65e14af0
Rogério Rodrigues Silva <114150848+RogerioRSilva@users.noreply.github.com>
2023-03-27 23:38:33 +0000

    Aula-27-03-2023 Criando conexão MongoDB

---
+++ back-end/config/database.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+/**
+ * Usa desestruturação para obter as variáveis de ambiente
+ * necessáras para realizar a conexão ao banco de dados. 
+ */
+
+const {
+    MONGODB_USER,
+    MONGODB_PASS,
+    MONGODB_SERVER,
+    MONGODB_DATABASE
+} = process.env
+
+module.exports = function(){
+
+    // Conecta ao banco de dados
+    mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_SERVER}/${MONGODB_DATABASE}?retryWrites=true&w=majority`,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    mongoose.connection.on('connected', () =>
+        console.log('=>MONGOOSE! conectado com sucesso ao servidor')
+    )
+
+    mongoose.connection.on('disconnected', () =>
+        console.log('=>MONGOOSE! desconectado do servidor')
+    )
+
+    mongoose.connection.on('error', error =>
+        console.error('*** MONGOOSE! ERRO ao conectar ao servidor' + error)
+    )
+
+    // Quando for detectado ocomando de interrupção Ctrl+C
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            console.log('=> MONGOOSE! Desconectando ...')
+            // Encerra a aplicação sem erros
+            process.exit(0)
+        })
+    })
+}
